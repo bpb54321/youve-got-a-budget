1d0069cde56662b3f250c97b7ab9f96c33825f0d
Brian Blosser <brian.blosser@fxinnovation.com>
2024-08-16 16:47:08 -0400

    Emulate ghost button from YNAB.

--- src/button/Button.tsx
+++ src/button/Button.tsx
@@ -1,48 +1,15 @@
-import React from 'react';
-import './button.css';
+import { ReactNode } from "react";
+import classes from "./button.module.css";
 
 export interface ButtonProps {
-  /**
-   * Is this the principal call to action on the page?
-   */
-  primary?: boolean;
-  /**
-   * What background color to use
-   */
-  backgroundColor?: string;
-  /**
-   * How large should the button be?
-   */
-  size?: 'small' | 'medium' | 'large';
-  /**
-   * Button contents
-   */
-  label: string;
-  /**
-   * Optional click handler
-   */
   onClick?: () => void;
+  children: ReactNode;
 }
 
-/**
- * Primary UI component for user interaction
- */
-export const Button = ({
-  primary = false,
-  size = 'medium',
-  backgroundColor,
-  label,
-  ...props
-}: ButtonProps) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+export const Button = ({ onClick, children }: ButtonProps) => {
   return (
-    <button
-      type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
-      style={{ backgroundColor }}
-      {...props}
-    >
-      {label}
+    <button onClick={onClick} type="button" className={classes.button}>
+      {children}
     </button>
   );
 };
